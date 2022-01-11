244aa6fbd39f2363267fd653c0dad7512460784d
Rythelle C. Pereira <rythelle20@hotmail.com>
2022-01-11 13:47:22 -0300

    Challenge Finished

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,45 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
-  findById(id: string): User | undefined {
-    // Complete aqui
+  findById(id: string): User {
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
   }
 
-  findByEmail(email: string): User | undefined {
-    // Complete aqui
+  findByEmail(email: string): User {
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
   }
 
-  turnAdmin(receivedUser: User): User {
-    // Complete aqui
+  turnAdmin({ id }: User): User {
+    const findIndex = this.users.findIndex((user) => user.id === id);
+
+    this.users[findIndex].admin = true;
+    this.users[findIndex].updated_at = new Date();
+
+    const userUpdated = this.users[findIndex];
+
+    return userUpdated;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not exists");
+    }
+
+    if (user.admin === true) {
+      const listUser = this.usersRepository.list();
+
+      return listUser;
+    }
+
+    throw new Error("User isn't admin");
   }
 }
 
